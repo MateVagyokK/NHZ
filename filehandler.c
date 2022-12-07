f69415d8f546a40c34e9932da9bd899ed8866994
Saskó Máté <sasko.mate.03@gmail.com>
2022-12-07 19:39:59 +0100

    finalize error handling in files

--- filehandler.c
+++ filehandler.c
@@ -9,6 +9,24 @@ Készítette: Saskó Máté
 #include <stdbool.h>
 #include <string.h>
 
+/* char *toEng(char *inp)
+{
+    int i = 0;
+    while (inp[i] != '\0')
+    {
+        switch (inp[i])
+        {
+        case 'á':
+            inp[i++] = 'a';
+            break;
+
+        default:
+            i++;
+            break;
+        }
+    }
+    return inp;
+} */
 /*Visszatérési érték nélküli függvény, mely elmenti a mentést tartalmazó txt-be a lista eseményeit.
 Paraméterként kéri a lista első elemét.
 Globálisan használható.*/
@@ -51,7 +69,7 @@ listItem *readFromFile(listItem *first)
     fptr = fopen("save.txt", "r");
     if (fptr == NULL)
     {
-        perror("Fájl megnyitása sikertelen");
+        perror("Fájl megnyitása sikertelen!\n A program letrehozza az uj mentesfajlt!");
         getchar();
         return NULL;
     }
@@ -60,6 +78,7 @@ listItem *readFromFile(listItem *first)
     fgets(buffer, 200, fptr);
     int n = 0;
     bool error = false;
+    bool asked = false;
     sscanf(buffer, "%d", &n);
     for (int i = 0; i < n; i++)
     {
@@ -73,11 +92,16 @@ listItem *readFromFile(listItem *first)
         if (sscanf(buffer, "[date]:%d.%d.%d.", &tmp.eventDate.year, &tmp.eventDate.month, &tmp.eventDate.day) != 3)
         {
             error = true;
+            tmp.eventDate.year = -1;
+            tmp.eventDate.month = -1;
+            tmp.eventDate.day = -1;
         }
         fgets(buffer, 200, fptr);
         if (sscanf(buffer, "[time]:%d:%d", &tmp.eventTime.hour, &tmp.eventTime.minute) != 2)
         {
             error = true;
+            tmp.eventTime.hour = -1;
+            tmp.eventTime.minute = -1;
         }
         fgets(buffer, 200, fptr);
         if (sscanf(buffer, "[place]:%[^\n]", tmp.place) != 1)
@@ -91,10 +115,27 @@ listItem *readFromFile(listItem *first)
             error = true;
             strcpy(tmp.desc, "");
         }
-        if (error)
+        if (error && !asked)
         {
-            printf("Hiba");
-            getchar();
+            bool running = true;
+            while (running)
+            {
+                printf("Hibas mentesfajl!\nA betoltendo adatok kozt valoszinuleg hiba szerepel.\nKivanja igy is betolteni?\n");
+                printf("1. Betoltes mellozese\n");
+                printf("2. Betoltes folytatasa\n");
+                int input;
+                scanf("%d", &input);
+                switch (input)
+                {
+                case 1:
+                    fclose(fptr);
+                    return NULL;
+                case 2:
+                    asked = true;
+                    running = false;
+                    break;
+                }
+            }
         }
         first = addListItem(first, tmp);
     }
